904c2d9f4ee88493b769e18664cdc860db7f8e1f
Michael Buchman <mbookie213@gmail.com>
2018-08-20 11:39:04 -0500

    Initialize Participant.java

---
+++ src/Participant.java
@@ -0,0 +1,14 @@
+public class Participant {
+
+    private String name, email;
+
+    /**
+    * Constructor for instantiating a participant
+    * @param name: The provided name for this participant
+    * @param email: The provided email for this participant
+    */
+    public Participant(String name, String email) {
+    	this.name = name;
+    	this.email = email;
+    }
+}
