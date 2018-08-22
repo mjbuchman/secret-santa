843f17037ca1e2ef9cb78e3bca1be835466fd929
Michael Buchman <mbookie213@gmail.com>
2018-08-22 09:40:46 -0500

    Initialize ParticipantList.java

--- src/ParticipantList.java
+++ src/ParticipantList.java
@@ -1,1 +1,17 @@
+import java.util.*;
 
+public class ParticipantList {
+
+    private ArrayList<Participant> list;
+
+    /**
+    * Constructor for instantiating a ParticipantList
+    * @param name: The provided name for this participant
+    * @param email: The provided email for this participant
+    */
+    public ParticipantList() {
+    	this.list = new ArrayList<>();
+    }
+    
+     
+}
