9dd8b026e6d612378ea7e24b4e27cc36fc2de181
Michael Buchman <mbookie213@gmail.com>
2018-08-22 13:17:02 -0500

    Added default methods

--- src/Participant.java
+++ src/Participant.java
@@ -1,14 +1,36 @@
 public class Participant {
 
     private String name, email;
+    private int target;
 
     /**
-    * Constructor for instantiating a participant
+    * Constructor for instantiating a job
     * @param name: The provided name for this participant
     * @param email: The provided email for this participant
     */
     public Participant(String name, String email) {
     	this.name = name;
     	this.email = email;
+    	this.target = -1;
+    }
+    
+    public String getName() {
+    	return this.name;
+    }
+    
+    public String getEmail() {
+    	return this.email;
+    }
+    
+    public int getTarget() {
+    	return this.target;
+    }
+    
+    public void setTarget(int target) {
+    	this.target = target;
+    }
+    
+    public void printResults(ParticipantList people) {
+    	System.out.println(this.getName() + " : " + this.getEmail() + " ---> " + people.getList().get(this.target).getName());
     }
 }
