4254204b7f1b5e10e48d8d3c8dd5fd921b28920b
Michael Buchman <mbookie213@gmail.com>
2018-08-22 10:20:29 -0500

    Added assignParticipants

--- src/ParticipantList.java
+++ src/ParticipantList.java
@@ -3,15 +3,26 @@ import java.util.*;
 public class ParticipantList {
 
     private ArrayList<Participant> list;
+    private Random randomGenerator;
 
     /**
-    * Constructor for instantiating a ParticipantList
-    * @param name: The provided name for this participant
-    * @param email: The provided email for this participant
+    * Constructor for instantiating a job
+    * @param list:
     */
     public ParticipantList() {
-    	this.list = new ArrayList<>();
+    	list = new ArrayList<>();
+        randomGenerator = new Random();
     }
     
-     
+    public void assignParticipants() {
+    	for (int i = 0; i < list.size(); i++) {
+    		while (list.get(i).getTarget() == -1) {
+    			int index = randomGenerator.nextInt(list.size());
+    			list.get(i).setTarget(index);
+    			if (list.get(i).getName().equals(list.get(index).getName())) {
+    				list.get(i).setTarget(-1);
+    			}
+    		}
+    	}
+    }
 }
