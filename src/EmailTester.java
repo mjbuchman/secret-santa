386c06a69a0111aa08d855c4b7119516b7bdb217
Michael Buchman <mbookie213@gmail.com>
2018-08-27 10:49:24 -0500

    Tests email sending capabilities

---
+++ src/EmailTester.java
@@ -0,0 +1,14 @@
+package secretsanta;
+
+public class EmailTester {
+	public static void main(String[] args) {
+		EmailSender test = new EmailSender("mbookie213@gmail.com", "Billy");
+		try {
+            test.sendHtmlEmail();
+            System.out.println("Email sent.");
+        } catch (Exception ex) {
+            System.out.println("Failed to send email.");
+        }
+	}
+
+}
