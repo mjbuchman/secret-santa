5de4299b6c7616cdba901c16fa56597bb0d11994
Michael Buchman <mbookie213@gmail.com>
2018-08-27 10:48:21 -0500

    Tests random assignment of participants

---
+++ src/AssignmentTester.java
@@ -0,0 +1,25 @@
+package secretsanta;
+
+public class AssignmentTester {
+
+	public static void main(String[] args) {
+		Participant p1 = new Participant("Bob", "bob@gmail.com");
+		Participant p2 = new Participant("Mike", "mike@gmail.com");
+		Participant p3 = new Participant("Tim", "tim@gmail.com");
+		Participant p4 = new Participant("Larry", "larry@gmail.com");
+		Participant p5 = new Participant("Jason", "jason@gmail.com");
+		Participant p6 = new Participant("David", "david@gmail.com");
+	
+		ParticipantList people = new ParticipantList();
+		people.add(p1);
+		people.add(p2);
+		people.add(p3);
+		people.add(p4);
+		people.add(p5);
+		people.add(p6);
+		
+		people.assignParticipants();
+		people.printResults();
+	}
+
+}
