29f150e490b8d3439a5251254d909cc886616e0e
Michael Buchman <mbookie213@gmail.com>
2018-08-28 09:16:58 -0500

    Style and commenting

--- src/Participant.java
+++ src/Participant.java
@@ -1,7 +1,7 @@
 public class Participant {
 
-    private String name, email;
-    private int target;
+    private String name, email;  // Strings containing identifying information
+    private int target;  // Index of participants assigned target
 
     /**
     * Constructor for instantiating a participant
@@ -14,22 +14,45 @@ public class Participant {
     	this.target = -1;
     }
     
+    /**
+    * Get method for name field
+    * 
+    * @return this object's name
+    */
     public String getName() {
     	return this.name;
     }
     
+    /**
+    * Get method for email field
+    * 
+    * @return this object's email
+    */
     public String getEmail() {
     	return this.email;
     }
     
+    /**
+    * Get method for target field
+    * 
+    * @return this object's target
+    */
     public int getTarget() {
     	return this.target;
     }
     
+    /**
+    * Set method for name field
+    * 
+    * @param target: integer to set this object's target to
+    */
     public void setTarget(int target) {
     	this.target = target;
     }
     
+    /**
+    * Print method for all fields of a Participant object
+    */
     public void print() {
     	System.out.println(this.getName() + " : " + this.getEmail() + " : " + this.getTarget());
     }
