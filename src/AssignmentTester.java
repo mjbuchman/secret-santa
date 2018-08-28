4b105f3d6b3a20c4953c39cd142ca044f7fbc8d5
Michael Buchman <mbookie213@gmail.com>
2018-08-28 09:11:05 -0500

    Style and commenting

--- src/AssignmentTester.java
+++ src/AssignmentTester.java
@@ -1,8 +1,8 @@
-package secretsanta;
-
 public class AssignmentTester {
 
 	public static void main(String[] args) {
+		
+		/* Create Participants */
 		Participant p1 = new Participant("Bob", "bob@gmail.com");
 		Participant p2 = new Participant("Mike", "mike@gmail.com");
 		Participant p3 = new Participant("Tim", "tim@gmail.com");
@@ -10,6 +10,7 @@ public class AssignmentTester {
 		Participant p5 = new Participant("Jason", "jason@gmail.com");
 		Participant p6 = new Participant("David", "david@gmail.com");
 	
+		/* Add Participants to new ParticipantList */
 		ParticipantList people = new ParticipantList();
 		people.add(p1);
 		people.add(p2);
@@ -18,6 +19,7 @@ public class AssignmentTester {
 		people.add(p5);
 		people.add(p6);
 		
+		/* Assign and print results */
 		people.assignParticipants();
 		people.printResults();
 	}
